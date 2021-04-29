288532d987375bc8039ff00f3c8b05e934db9f80
Mohit Bhole <43913328+moiiiiit@users.noreply.github.com>
2021-04-29 03:10:14 -0500

    add backend code and connect to frontend

--- backend/radiant-backend.js
+++ backend/radiant-backend.js
@@ -1,14 +1,36 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
+const port = 9505;
+var todos = [];
+var complete = [];
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+    next();
+  });
+  
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/getTodos", (request, response) => response.status(200).json(todos));
+app.get("/getComplete", (request, response) => response.status(200).json(complete));
+app.post("/postNewTodo", (request, response) => {
+    var newTodo = request.body;
+    if(newTodo.complete == 1){
+        complete.push(newTodo);
+        todos = todos.filter(x => x.id != newTodo.id);
+    }
+    else{
+        todos.push(newTodo);
+    }
+    response.status(201).json(newTodo);
+  });
+
+app.listen(port, function () {
+  console.log("Radiant listening on port 9505!");
 });
--- radiant/src/pages/home.js
+++ radiant/src/pages/home.js
@@ -3,46 +3,15 @@ import {
   Button,
   Row,
   Col,
-  DatePicker,
   Input,
   PageHeader,
   Divider,
-  Calendar,
-  Card,
   List,
   Typography,
   Space,
-  Avatar,
-  Menu,
 } from "antd";
-import Logo from "../assets/logo.jpg";
 const { Title, Text } = Typography;
 const { TextArea } = Input;
-const { SubMenu } = Menu;
-const completed = [
-  {
-    time: "3:00pm",
-    title: "Title 1",
-    desc: "Hi",
-  },
-  {
-    time: "3:00pm",
-    title: "Title 2",
-    desc: "Hi",
-  },
-];
-const todo = [
-  {
-    time: "3:00pm",
-    title: "Title 1",
-    desc: "Hi",
-  },
-  {
-    time: "3:00pm",
-    title: "Title 2",
-    desc: "Hi",
-  },
-];
 
 class Home extends React.Component {
   constructor() {
@@ -51,11 +20,17 @@ class Home extends React.Component {
       height: 0,
       width: 0,
       currentMenuItem: "1",
+      completed: [],
+      todo: [],
+      temptitle: "",
+      tempdesc: "",
     };
     window.addEventListener("resize", this.update);
   }
   componentDidMount() {
     this.update();
+    this.getComplete();
+    this.getTodos();
   }
 
   update = () => {
@@ -64,67 +39,244 @@ class Home extends React.Component {
       width: window.innerWidth,
     });
   };
+
+  async getComplete() {
+    const response = await fetch("http://127.0.0.1:9505/getComplete");
+    const data = await response.json();
+    console.log(data);
+    this.setState({ completed: data });
+  }
+
+  async getTodos() {
+    const response = await fetch("http://127.0.0.1:9505/getTodos");
+    const data = await response.json();
+    this.setState({ todo: data });
+  }
+
+  async postTodo(todo) {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    };
+    const response = await fetch(
+      "http://127.0.0.1:9505/postNewTodo",
+      requestOptions
+    );
+    const data = await response.json();
+    console.log(data);
+
+    this.getTodos();
+    this.getComplete();
+  }
+
   render() {
     return (
       <div>
-        <PageHeader
-          title={<Title>Radiant</Title>}
-          subTitle="A Simple To-Do List"
-          extra={[
-            <Text type="secondary">Author: Mohit Bhole</Text>,
-            <Button key="3" type="link">My Github</Button>,
-            <Button key="4" type="link">My Portfolio</Button>,
-          ]}
-        />
-        <Row gutter={[16, 16]}>
-          <Col span={12}>
+        <div style={{ paddingTop: 12, paddingRight: 12, paddingLeft: 12 , backgroundColor: "#f5f5f5", marginBottom: this.state.height*0.04 }}>
+          <PageHeader
+            title={<Title>Radiant</Title>}
+            subTitle="A Simple To-Do List"
+            extra={[
+              <Text type="secondary">Author: Mohit Bhole</Text>,
+              <Button key="3" type="link">
+                My Github
+              </Button>,
+              <Button key="4" type="link">
+                My Portfolio
+              </Button>,
+            ]}
+          />
+        </div>
+
+        {this.state.width >= this.state.height ? (
+          //Desktop Orientation
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+              <Space direction="vertical" style={{ width: "80%" }}>
+                <Title level={2}>Create a new task</Title>
+                <Input
+                  placeholder="Name your task"
+                  value={this.state.temptitle}
+                  onChange={(e) => {
+                    this.setState({ temptitle: e.target.value });
+                  }}
+                ></Input>
+                <TextArea
+                  autoSize={{ minRows: 4, maxRows: 10 }}
+                  placeholder="Describe your task"
+                  value={this.state.tempdesc}
+                  onChange={(e) => {
+                    this.setState({ tempdesc: e.target.value });
+                  }}
+                ></TextArea>
+                <Space>
+                  <Button
+                    onClick={() => {
+                      if (this.state.temptitle) {
+                        this.postTodo({
+                          id: Math.random().toString(36).slice(2),
+                          title: this.state.temptitle,
+                          description: this.state.tempdesc
+                            ? this.state.tempdesc
+                            : "",
+                          complete: 0,
+                        });
+                        this.setState({ tempdesc: "", temptitle: "" });
+                      }
+                    }}
+                    type="primary"
+                  >
+                    Add task
+                  </Button>
+                </Space>
+                <Divider></Divider>
+                <Title level={2}>Completed tasks</Title>
+                <List
+                  size="large"
+                  bordered
+                  dataSource={this.state.completed}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <List.Item.Meta
+                        title={item.title}
+                        description={item.description}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </Space>
+            </Col>
+            <Col span={12}>
+              <Space direction="vertical" style={{ width: "80%" }}>
+                <Title level={2}>To-Do List</Title>
+                <List
+                  size="large"
+                  bordered
+                  dataSource={this.state.todo}
+                  renderItem={(item) => (
+                    <List.Item
+                      extra={
+                        <Button
+                          onClick={() => {
+                            this.postTodo({
+                              id: item.id,
+                              title: item.title,
+                              description: item.description
+                                ? item.description
+                                : "",
+                              complete: 1,
+                            });
+                          }}
+                        >
+                          Done
+                        </Button>
+                      }
+                    >
+                      <List.Item.Meta
+                        title={item.title}
+                        description={item.description}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </Space>
+            </Col>
+          </Row>
+        ) : (
+          //Mobile Orientation
+          <div>
             <Space direction="vertical" style={{ width: "80%" }}>
               <Title level={2}>Create a new task</Title>
-              <Input placeholder="Name your task"></Input>
+              <Input
+                placeholder="Name your task"
+                value={this.state.temptitle}
+                onChange={(e) => {
+                  this.setState({ temptitle: e.target.value });
+                }}
+              ></Input>
               <TextArea
                 autoSize={{ minRows: 4, maxRows: 10 }}
                 placeholder="Describe your task"
+                value={this.state.tempdesc}
+                onChange={(e) => {
+                  this.setState({ tempdesc: e.target.value });
+                }}
               ></TextArea>
               <Space>
-                <DatePicker showTime></DatePicker>
-                <Button type="primary">Add task</Button>
+                <Button
+                  onClick={() => {
+                    if (this.state.temptitle) {
+                      this.postTodo({
+                        id: Math.random().toString(36).slice(2),
+                        title: this.state.temptitle,
+                        description: this.state.tempdesc
+                          ? this.state.tempdesc
+                          : "",
+                        complete: 0,
+                      });
+                      this.setState({ tempdesc: "", temptitle: "" });
+                    }
+                  }}
+                  type="primary"
+                >
+                  Add task
+                </Button>
+              </Space>
+              <Divider></Divider>
+              <Space direction="vertical" style={{ width: "80%" }}>
+                <Title level={2}>To-Do List</Title>
+                <List
+                  size="large"
+                  bordered
+                  dataSource={this.state.todo}
+                  renderItem={(item) => (
+                    <List.Item
+                      extra={
+                        <Button
+                          onClick={() => {
+                            this.postTodo({
+                              id: item.id,
+                              title: item.title,
+                              description: item.description
+                                ? item.description
+                                : "",
+                              complete: 1,
+                            });
+                          }}
+                        >
+                          Done
+                        </Button>
+                      }
+                    >
+                      <List.Item.Meta
+                        title={item.title}
+                        description={item.description}
+                      />
+                    </List.Item>
+                  )}
+                />
               </Space>
               <Divider></Divider>
               <Title level={2}>Completed tasks</Title>
               <List
                 size="large"
                 bordered
-                dataSource={completed}
+                dataSource={this.state.completed}
                 renderItem={(item) => (
-                  <List.Item extra={<Button>Done</Button>}>
+                  <List.Item>
                     <List.Item.Meta
-                      title={item.time + " - " + item.title}
-                      description={item.desc}
+                      title={item.title}
+                      description={item.description}
                     />
                   </List.Item>
                 )}
               />
             </Space>
-          </Col>
-          <Col span={12}>
-            <Space direction="vertical" style={{ width: "80%" }}>
-              <Title level={2}>To-Do List</Title>
-              <List
-                size="large"
-                bordered
-                dataSource={todo}
-                renderItem={(item) => (
-                  <List.Item extra={<Button>Done</Button>}>
-                    <List.Item.Meta
-                      title={item.time + " - " + item.title}
-                      description={item.desc}
-                    />
-                  </List.Item>
-                )}
-              />
-            </Space>
-          </Col>
-        </Row>
+          </div>
+        )}
       </div>
     );
   }
